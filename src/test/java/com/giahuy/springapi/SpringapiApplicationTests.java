a1f29ae90fb9b30c9c60747c4e50c41b5a790d9c
Huy Ngo <ngoducgiahuy@gmail.com>
2021-06-29 22:18:10 +0700

    Create project

---
+++ src/test/java/com/giahuy/springapi/SpringapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giahuy.springapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
