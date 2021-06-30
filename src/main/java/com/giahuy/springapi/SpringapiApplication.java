b225e6cf9d6196ffa531a0e2988abd068130876a
Huy Ngo <ngoducgiahuy@gmail.com>
2021-06-30 17:32:31 +0700

    add @EnableJpaAuditing anotation

--- src/main/java/com/giahuy/springapi/SpringapiApplication.java
+++ src/main/java/com/giahuy/springapi/SpringapiApplication.java
@@ -2,8 +2,10 @@ package com.giahuy.springapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringapiApplication {
 
 	public static void main(String[] args) {
